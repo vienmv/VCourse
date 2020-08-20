c5cc75525fc8ae946fc346f925d1c8076b3d11dc
vienmv.dev@gmail.com <UocmoFPT1998##>
2020-08-21 06:24:08 +0700

    First commit

---
+++ src/main/java/com/vienmv/vfarm/VfarmApplication.java
@@ -0,0 +1,12 @@
+package com.vienmv.vfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VfarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VfarmApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vienmv/vfarm/VfarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vienmv.vfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VfarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
